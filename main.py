5e401df0176185823015da043e6575a79d31848c
Inna.Rakowski <Inna.Rakowski@soft-project.pl>
2023-06-28 12:43:07 +0200

    import

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from . import constants
+import constants
 
 df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],index=['row 1', 'row 2'],
 
                    columns=['col 1', 'col 2'])
-
 df1.to_excel(constants.OUTPUT_PATH)
