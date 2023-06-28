22f683ef1c67e069c22a3cd689ffbbddc3b504a1
irakowski <irakowski>
2023-06-28 15:20:51 +0000

    dbs commit
    commit from databrics

---
+++ workspace_init.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+pip install -r requirements.txt
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import openpyxl
+
+# COMMAND ----------
+
+#%%
+import pandas as pd
+import constants
+
+
+#%%
+df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],index=['row 1', 'row 2'],
+
+                   columns=['col 1', 'col 2'])
+df1.to_excel(constants.OUTPUT_PATH)
+
+# COMMAND ----------
+
+
